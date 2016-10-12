33b29234c93ab5f23c2813a7ab0e7f9e55a1bdb7
ammyblabla <ammyblabla@ammyblabla>
2016-10-12 20:59:19 +0700

    วาดหม้อ

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,14 +1,24 @@
 package com.mygdx.game;
-import com.badlogic.gdx.ScreenAdapter;;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameScreen extends ScreenAdapter {
 	private ShabuGame shabuGame;
-	
+	private Texture shabuImg;
 	public GameScreen(ShabuGame shabugame){
 		this.shabuGame = shabugame;
+		shabuImg = new Texture("background.png");
 	}
 	
 
 	public void render(float delta){
 		System.out.println(delta);
+		SpriteBatch batch = shabuGame.batch;
+		batch.begin();
+		batch.draw(shabuImg,0,0);
+		batch.end();
+	}
+	public void drawBg(){
+		
 	}
 }
